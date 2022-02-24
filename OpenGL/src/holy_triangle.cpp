96c3c2aec4d9961dea754256c09ab1c74f3ca204
Edwin Joy <edwinjoy7026@live.com>
2022-02-24 13:17:53 +0530

    Moved shaders to a different file

--- OpenGL/src/holy_triangle.cpp
+++ OpenGL/src/holy_triangle.cpp
@@ -2,15 +2,57 @@
  *@file holy_triangle.cpp
  *@name Edwin Joy
  *@email edwin7026@gmail.com
-*/
+**/
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+
+    enum class ShaderType
+    {
+        NONE = -1,
+        VERTEX = 0,
+        FRAGMENT = 1
+    };
+
+    ShaderType type = ShaderType::NONE;
+    std::string line;
+    std::stringstream ss[2];
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+                type = ShaderType::VERTEX;
+            else if (line.find("fragment") != std::string::npos)
+                type = ShaderType::FRAGMENT;
+        }
+        else
+        {
+            ss[(int)type] << line << "\n";
+        }
+    }
+
+    return { ss[0].str(), ss[1].str() };
+}
 
 /**
- * 
+ * Function to compile shaders
+ * Also handles errors if any error is found during compilation
  */
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -107,31 +149,13 @@ int main(void)
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
     /* The shader code goes here*/
-
-    // Vertex shader describes where to render the triangle
-    std::string vertexShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = position;\n"
-        "}\n";
-
-    // Fragment shader basically descries the coloring and rendering of
-    // individual pixels in the triangle
-    std::string fragmentShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) out vec4 color;\n"
-        "void main()\n"
-        "{\n"
-        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n";
-
+    ShaderProgramSource source = ParseShader("res/shaders/Basic.shader"); 
+    
     // Create shader and get shader ID
-    unsigned int shader = CreateShader(vertexShader, fragmentShader);
-    glUseProgram(shader);               // Tell OpenGL specification to use that particular shader
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+
+    // Tell OpenGL specification to use that particular shader
+	glUseProgram(shader);               
 
 
 	/* Loop until the user closes the window */
